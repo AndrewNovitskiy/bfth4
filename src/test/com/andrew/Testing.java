39b35fd1b2e27bdf68e05734a66eeb738f5df156
Andrey <starwars@tut.by>
2017-04-30 18:45:08 +0300

    ->V1

--- src/test/com/andrew/Testing.java
+++ src/test/com/andrew/Testing.java
@@ -8,13 +8,12 @@ import org.junit.Test;
 import static org.junit.Assert.fail;
 
 public class Testing {
-    private static final Logger log = Logger.getLogger(Testing.class);
 
+    private static final Logger LOG = Logger.getLogger(Testing.class);
 
     @Ignore
     @Test
     public void testMethod() {
-        log.info("info");
         fail("It's a test fail! Don't worry!");
     }
 
